e6432d72e083e8436016a5d0ebde6c72ece20658
unknown <nil26566@gmail.com>
2025-06-17 06:30:39 +0600

    commit

--- src/Pages/ManegerUser.jsx
+++ src/Pages/ManegerUser.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Provider.jsx/AuthProvider';
 
 const ManegerUser = () => {
+    const {user}=useContext(AuthContext)
+    const [course,setCourse]=useState([])
+      useEffect(() => {
+        if (email) {
+            fetch(`http://localhost:3000/course/${email}`)
+                .then(res => res.json())
+                .then(data => {
+                    setCourse(data);
+                    console.log('data is', data);
+                });
+        }
+    }, [email]);
     return (
         <div>
             
